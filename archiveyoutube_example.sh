6bd6cef5aabcdfcafa3c3e4b427e197fed1c3514
Kieran Gee <kieran.lost.the.game@gmail.com>
2025-09-13 11:29:53 +0800

    better

---
+++ archiveyoutube_example.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+export PATH="/opt/ffmpeg:$PATH"
+cd "$(dirname "$0")"
+
+uv venv
+source .venv/bin/activate
+uv sync --upgrade
+
+
+apikey=AAA
+
+# To get chennel id look for in the page source of a channel
+
+# NPR
+python -m vod_archive -k $apikey -c "UC4eYXhJI4-7wSWc8UNRwD4A" -s "Tiny Desk" -w -p /srv/Freya/Video/Concerts/_NPR/ -n 100000
+
+echo -------------------------------------------------------------------------------------
+
+# KEXP
+python -m vod_archive -k $apikey -c "UC3I2GFN_F8WudD_2jUZbojA" -s "Full Performance" -w -p /srv/Freya/Video/Concerts/_KEXP/ -n 10000
